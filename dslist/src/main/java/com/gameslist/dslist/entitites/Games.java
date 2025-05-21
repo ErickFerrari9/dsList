20df2ab0035565024d6b70c03930247f46ee8bce
Erick <ferrari_erick@hotmail.com>
2025-05-21 14:51:46 -0400

    Game seed

--- dslist/src/main/java/com/gameslist/dslist/entitites/Games.java
+++ dslist/src/main/java/com/gameslist/dslist/entitites/Games.java
@@ -8,29 +8,41 @@ import java.util.Objects;
 @Table(name = "tb_game")
 public class Games {
 
+    private String imgUrl;
+    private String title;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String title;
-    private String genre;
-    private String platforms;
-    private String imagUrl;
-    private String shortDescription;
-    private String longDescription;
+
     @Column(name = "game_year")
     private Integer year;
+
+
+    private String genre;
+    private String platforms;
+    private Double score;
+
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+
 
     public Games() {
     }
 
-    public Games(long id, String title, Integer year, String genre, String platforms, String imagUrl,
+    public Games(long id, String title, Integer year, String genre, String platforms, Double score, String imagUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
-        this.imagUrl = imagUrl;
+        this.score = score;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -74,13 +86,19 @@ public class Games {
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
     }
-
-    public String getImagUrl() {
-        return imagUrl;
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
     }
 
-    public void setImagUrl(String imagUrl) {
-        this.imagUrl = imagUrl;
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
